5439bec4b1a4de2b71ba093787360b550bdf1c73
Danilo Rodrigues Teixeira <danilo.rt@outlook.com>
2021-10-01 19:22:33 -0300

    Criação do layout do formulário

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -6,9 +6,15 @@ import { FormsComponent } from './forms.component';
 import { TemplateComponent } from './template';
 import { ReactiveComponent } from './reactive';
 import { FormsModule } from '@angular/forms';
+import { FormDebugComponent } from './form-debug';
 
 @NgModule({
-  declarations: [FormsComponent, TemplateComponent, ReactiveComponent],
+  declarations: [
+    FormsComponent,
+    TemplateComponent,
+    ReactiveComponent,
+    FormDebugComponent,
+  ],
   imports: [CommonModule, FormsRoutingModule, FormsModule],
   exports: [TemplateComponent, ReactiveComponent],
 })
